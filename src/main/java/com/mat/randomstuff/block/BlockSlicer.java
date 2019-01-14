e027577209093d68752a71ffbf4dae4ecd079c46
matcool <26722564+matcool@users.noreply.github.com>
2019-01-14 16:23:54 -0300

    slicer block has textures and can rotate

--- src/main/java/com/mat/randomstuff/block/BlockSlicer.java
+++ src/main/java/com/mat/randomstuff/block/BlockSlicer.java
@@ -1,11 +1,46 @@
 package com.mat.randomstuff.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockSlicer extends BlockBase {
+	
+	public static final PropertyDirection PROPERTYFACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
 	public BlockSlicer() {
 		super(Material.ROCK, "Slicer");
 	}
 
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+	    EnumFacing facing = EnumFacing.getHorizontal(meta);
+	    return this.getDefaultState().withProperty(PROPERTYFACING, facing);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+	    EnumFacing facing = (EnumFacing)state.getValue(PROPERTYFACING);
+	    int facingbits = facing.getHorizontalIndex();
+	    return facingbits;
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] {PROPERTYFACING});
+	}
+	
+	@Override
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing blockFaceClickedOn, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+	    // find the quadrant the player is facing
+	    EnumFacing enumfacing = (placer == null) ? EnumFacing.NORTH : EnumFacing.fromAngle(placer.rotationYaw);
+
+	    return this.getDefaultState().withProperty(PROPERTYFACING, enumfacing);
+	}
 }
