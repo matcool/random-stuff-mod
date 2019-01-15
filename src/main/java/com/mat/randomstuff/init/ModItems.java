d3660b593e16cdea3b1a633b7ebbf24733b7e818
matcool <26722564+matcool@users.noreply.github.com>
2019-01-14 23:34:43 -0300

    "better" way of registering items still need to deal with the casting mess tho

--- src/main/java/com/mat/randomstuff/init/ModItems.java
+++ src/main/java/com/mat/randomstuff/init/ModItems.java
@@ -1,5 +1,6 @@
 package com.mat.randomstuff.init;
 
+import com.mat.randomstuff.RandomStuff;
 import com.mat.randomstuff.item.ItemBase;
 import com.mat.randomstuff.item.ItemLettuceSeeds;
 
@@ -9,21 +10,32 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModItems {
 	
 	public static ItemBase choppedPotato = new ItemBase("chopped_potato");
+	
 	public static ItemLettuceSeeds lettuceSeeds = new ItemLettuceSeeds();
 	public static ItemBase lettuce = new ItemBase("lettuce");
-
-	public static void register(IForgeRegistry<Item> registry) {
-		registry.registerAll(
+	
+	public static Object[] Items = {
 			choppedPotato,
 			lettuceSeeds,
-			lettuce
-		);
+			lettuce,
+	};
+
+	public static void register(IForgeRegistry<Item> registry) {
+		for (Object item : Items) {
+			registry.register((Item) item);
+		}
 	}
 	
 	public static void registerModels() {
-		choppedPotato.registerItemModel();
-		lettuceSeeds.registerItemModel();
-		lettuce.registerItemModel();
+		//TODO figure out a way that doesn't require a bunch of if statements
+		for (Object item : Items) {
+			if (item instanceof ItemBase) {
+				((ItemBase) item).registerItemModel();
+			}
+			if (item instanceof ItemLettuceSeeds) {
+				((ItemLettuceSeeds) item).registerItemModel();
+			}
+		}
 	}
 
 }
